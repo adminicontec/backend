eae22e9d7ac0dbff5ef8383e63fcdba8b6344f95
Giovanni García <ggarcia.icontec@poligran.edu.co>
2021-02-15 08:32:06 -0500

    Add fields to Curriculum Vitae fields for Usermodel

---
+++ src/app/models/skillTypeModel.ts
@@ -0,0 +1,34 @@
+// @import_dependencies_node Import libraries
+import mongoose_delete from "mongoose-delete";
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+// @end
+
+const SkillTypeSchema = new Schema({
+  // @add_schema Add schema here
+  name: {
+    type: Schema.Types.String,
+    required: true,
+  }
+  // @end
+}, {
+  collection: 'skill_types' ,timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+});
+
+// INFO: Para usar soft delete se debe invocar exactamente el metodo delete() o sus derivados en lugar de remove()
+// Example: UserModel.delete({_id: id})
+SkillTypeSchema.plugin(mongoose_delete,{
+  deletedAt : true,
+  overrideMethods: 'all',
+  indexFields: 'all'
+});
+
+// INFO: Si desea declarar los campos del esquema que no se tendrán en cuenta para la descripción general del modelo
+// SkillTypeSchema.methods.invalid_fields = ["field1", "field2"];
+
+// INFO: Si desea implementar un metodo despues de guardar habilite el siguiente metodo
+// SkillTypeSchema.methods.postSave = (parameters) => {
+// }
+
+
+export const SkillTypeModel = mongoose.model('SkillType', SkillTypeSchema);
--- src/app/models/userModel.ts
+++ src/app/models/userModel.ts
@@ -7,77 +7,77 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
   // @add_schema Add schema here
   userName: {
-    type:  Schema.Types.String,
+    type: Schema.Types.String,
     required: true,
   },
   normalizedUserName: {
-    type:  Schema.Types.String,
+    type: Schema.Types.String,
     // required: true,
   },
   email: {
-    type:  Schema.Types.String,
+    type: Schema.Types.String,
     required: true,
   },
   normalizedEmail: {
-    type:  Schema.Types.String,
+    type: Schema.Types.String,
     // required: true,
   },
   emailConfirmed: {
-    type:  Schema.Types.Boolean,
+    type: Schema.Types.Boolean,
     default: false
   },
   passwordHash: {
-    type:  Schema.Types.String,
+    type: Schema.Types.String,
     required: true,
   },
   securityStamp: {
-    type:  Schema.Types.String
+    type: Schema.Types.String
   },
   concurrencyStamp: {
-    type:  Schema.Types.String
+    type: Schema.Types.String
   },
   phoneNumber: {
-    type:  Schema.Types.String
+    type: Schema.Types.String
   },
   phoneNumberConfirmed: {
-    type:  Schema.Types.Boolean,
+    type: Schema.Types.Boolean,
     default: false
   },
   twoFactorEnabled: {
-    type:  Schema.Types.Boolean,
+    type: Schema.Types.Boolean,
     default: false
   },
   lockoutEnd: {
-    type:  Schema.Types.Date
+    type: Schema.Types.Date
   },
   lockoutEnabled: {
-    type:  Schema.Types.Boolean,
+    type: Schema.Types.Boolean,
     default: false
   },
   accessFailedCount: {
-    type:  Schema.Types.Number,
+    type: Schema.Types.Number,
     default: 0
   },
   profile: {
     name: {
-      type:  Schema.Types.String
+      type: Schema.Types.String
     },
     lastName: {
-      type:  Schema.Types.String
+      type: Schema.Types.String
     },
     avatarImageUrl: {
-      type:  Schema.Types.String
+      type: Schema.Types.String
     },
     city: {
-      type:  Schema.Types.String
+      type: Schema.Types.String
     },
     country: {
       type: Schema.Types.ObjectId,
       ref: "Country"
     },
-    timezone: {type: Schema.Types.String, default: 'GMT-5'},
-    culture: {type: Schema.Types.String, default: 'es_CO'},
-    screen_mode: { type: Schema.Types.String, default: 'light-mode'},
+    timezone: { type: Schema.Types.String, default: 'GMT-5' },
+    culture: { type: Schema.Types.String, default: 'es_CO' },
+    screen_mode: { type: Schema.Types.String, default: 'light-mode' },
   },
   roles: [{
     type: Schema.Types.ObjectId,
@@ -92,10 +92,11 @@ const UserSchema = new Schema({
     ref: "User"
   },
   curriculum_vitae: {
-    biography: { type: Schema.Types.String},
+    biography: { type: Schema.Types.String },
     laboral_experience: [
       {
         company_position: { type: Schema.Types.String },
+        description: { type: Schema.Types.String },
         company: { type: Schema.Types.String },
         start_date: { type: Schema.Types.Date },
         end_date: { type: Schema.Types.Date },
@@ -104,23 +105,33 @@ const UserSchema = new Schema({
     ],
     academic_info: [
       {
-        school: { type: Schema.Types.String },
+        institution: { type: Schema.Types.String },
         degree: { type: Schema.Types.String },
         start_date: { type: Schema.Types.Date },
         end_date: { type: Schema.Types.Date },
         currently: { type: Schema.Types.Boolean },
       }
+    ],
+    skill: [
+      {
+        name: { type: Schema.Types.String},
+        score: { type: Schema.Types.Number},
+        skill_type: {
+          type: Schema.Types.ObjectId,
+          ref: "SkillType"
+        },
+      }
     ]
   }
   // @end
 }, {
-  collection: 'users' ,timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+  collection: 'users', timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 });
 
 // INFO: Para usar soft delete se debe invocar exactamente el metodo delete() o sus derivados en lugar de remove()
 // Example: UserModel.delete({_id: id})
-UserSchema.plugin(mongoose_delete,{
-  deletedAt : true,
+UserSchema.plugin(mongoose_delete, {
+  deletedAt: true,
   overrideMethods: 'all',
   indexFields: 'all'
 });
