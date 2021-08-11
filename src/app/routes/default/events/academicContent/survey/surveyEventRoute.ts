5f652eb98d5271604f335b55abcfd7f1ef5147a1
Nicolas Guerrero <nicolasf1402@gmail.com>
2021-08-10 21:35:40 -0500

    endpoint

---
+++ src/app/routes/default/events/academicContent/survey/surveyEventRoute.ts
@@ -0,0 +1,38 @@
+// @import_dependencies_node Import libraries
+// @end
+
+// @import_controller Import controller
+import { DefaultEventsAcademicContentSurveySurveyEventController as Controller } from "@scnode_app/controllers/default/events/academicContent/survey/surveyEventController";
+// @end
+
+// @import_utilities Import utilities
+import { routerUtility } from "@scnode_core/utilities/routerUtility";
+// @end
+
+class SurveyEventRoute {
+
+  private router_prefix: string = '/event/survey'; //Ej: /user
+
+  // @instance_controller
+  public instanceController: Controller = new Controller();
+  // @end
+
+  constructor () {}
+
+  /**
+  * Metodo que permite agregar rutas especificas por controlador
+  * @param app Objeto de clase Express Aplication
+  * @param [prefix] Prefijo para el enrutamiento
+  */
+  public routes(app,prefix: string = '/'): void {
+
+    const _route = `${prefix}${this.router_prefix}`;
+
+    // @add_routes Add routes: Ej: routerUtility.get(app,_route,'/url-for-request',this.instanceController.method,[{middleware: 'middleware-name', method: 'method-name'}...],[...]);
+    routerUtility.get(app, _route, '/check-survey-available/:moodle_id', this.instanceController.checkSurveyAvailable, [], ['auth'])
+    // @end
+  }
+}
+
+export const surveyEventRoute = new SurveyEventRoute();
+export { SurveyEventRoute as DefaultEventsAcademicContentSurveySurveyEventRoute };
---
+++ src/app/services/default/events/academicContent/survey/surveyEventService.ts
@@ -0,0 +1,131 @@
+// @import_dependencies_node Import libraries
+const ObjectID = require('mongodb').ObjectID
+import moment from 'moment'
+// @end
+
+// @import services
+import { userService } from '@scnode_app/services/default/admin/user/userService';
+// @end
+
+// @import utilities
+import { responseUtility } from '@scnode_core/utilities/responseUtility';
+// @end
+
+// @import models
+import { CourseSchedulingDetails, Enrollment, Survey } from '@scnode_app/models';
+// @end
+
+// @import types
+import {ICheckSurveyAvailable} from '@scnode_app/types/default/events/academicContent/survey/surveyEventTypes'
+import { QueryValues } from '@scnode_app/types/default/global/queryTypes';
+// @end
+
+class SurveyEventService {
+
+  /*===============================================
+  =            Estructura de un metodo            =
+  ================================================
+    // La estructura de un metodo debe ser la siguiente:
+    public methodName = () => {}
+  /*======  End of Estructura de un metodo  =====*/
+
+  constructor () {}
+
+  /**
+   * Metodo que permite consultar si una encuesta esta disponible
+   * @param params
+   * @returns
+   */
+  public checkSurveyAvailable = async (params: ICheckSurveyAvailable) => {
+    try {
+      const today = moment.utc()
+
+      // @INFO: Validando el usuario
+      const userResponse: any = await userService.findBy({query: QueryValues.ONE, where: [{'field': '_id', 'value': params.user}]})
+      if (userResponse.status === 'error') return userResponse
+
+      // @INFO: Validando el programa
+      const enrollment = await Enrollment.findOne({user: params.user, courseID: params.moodle_id})
+      .select('id course_scheduling')
+      .populate({path: 'course_scheduling', select: 'id schedulingMode startDate endDate', populate: {
+        path: 'schedulingMode', select: 'id name'
+      }})
+      .lean()
+
+      if (!enrollment) return responseUtility.buildResponseFailed('json', null, {error_key: ''})
+
+      let surveyAvailable = false
+      // TODO: Consultar la programación
+      // En virtual va dirigido al programa y en online y presencial a cada curso
+      const schedulingMode = enrollment.course_scheduling.schedulingMode.name
+      if (schedulingMode === 'Presencial' || schedulingMode === 'En linea') {
+        // TODO: Fechas de los cursos
+        const detailScheduling = await CourseSchedulingDetails.find({
+          course_scheduling: enrollment.course_scheduling._id,
+          endDate: {$lt: today}
+        }).select('id startDate endDate')
+        .lean()
+        .sort({startDate: 1})
+
+        if (detailScheduling.length === 0) return responseUtility.buildResponseFailed('json') // TODO: Pendiente validacion
+        console.log('detailScheduling', detailScheduling)
+
+        surveyAvailable = true
+      } else if (schedulingMode === 'Virtual') {
+        // TODO: Fechas del programa
+        const endDate = moment.utc(enrollment.course_scheduling.endDate)
+        console.log('endDate', endDate)
+        if (today.isSameOrBefore(endDate)) {
+          surveyAvailable = true
+        } else {
+          return responseUtility.buildResponseFailed('json') // TODO: Pendiente validacion
+        }
+      }
+
+      if (surveyAvailable === false) return responseUtility.buildResponseFailed('json') // TODO: Pendiente validacion
+
+      // TODO: Verificar si ya se ejecuto la encuesta
+
+      // console.log('schedulingMode', schedulingMode)
+
+      const aggregateQuery = [
+        {
+          $lookup: {
+            from: 'academic_resource_configs',
+            localField: 'config.content',
+            foreignField: '_id',
+            as: 'config.content'
+          }
+        },
+        { $unwind: '$config.content' },
+        {
+          $match: {
+            'config.content.config.course_modes': ObjectID(enrollment.course_scheduling.schedulingMode._id),
+          }
+        },
+        {
+          $group: {
+            _id: '$_id',
+            survey: { "$first": '$_id'},
+            academic_resource_config: {$first: '$config.content._id'}
+          }
+        }
+      ]
+
+      const data = await Survey.aggregate(aggregateQuery)
+
+      if (data.length === 0) return responseUtility.buildResponseFailed('json') // TODO: Pendiente
+      console.log('data', data)
+
+      return responseUtility.buildResponseSuccess('json', null, {additional_parameters: {
+        survey: data[0].survey,
+        academic_resource_config: data[0].academic_resource_config,
+      }})
+    } catch (error) {
+      return responseUtility.buildResponseFailed('json')
+    }
+  }
+}
+
+export const surveyEventService = new SurveyEventService();
+export { SurveyEventService as DefaultEventsAcademicContentSurveySurveyEventService };
