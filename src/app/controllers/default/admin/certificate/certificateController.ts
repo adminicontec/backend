98cc24244253c38caa5f5e8675302fff47d115b2
Giovanni García <ggarcia.icontecpoligran.edu.co>
2021-11-10 16:00:22 -0500

    First part of Certificate generation using "Huella de Confianza" web service.

---
+++ src/app/controllers/default/admin/certificate/certificateController.ts
@@ -0,0 +1,45 @@
+// @import_dependencies_node Import libraries
+import { Request, Response } from 'express';
+// @end
+
+// @import_models Import models
+// @end
+
+// @import_services Import services
+import {certificateService} from '@scnode_app/services/default/huellaDeConfianza/certificate/certificateService'
+// @end
+
+// @import_utilities Import utilities
+import { responseUtility } from "@scnode_core/utilities/responseUtility";
+import { requestUtility } from "@scnode_core/utilities/requestUtility";
+// @end
+
+// @import_types Import types
+// @end
+
+class CertificateController {
+
+  /*===============================================
+  =            Estructura de un metodo            =
+  ================================================
+    // La estructura de un metodo debe ser la siguiente:
+    public methodName = (req: Request, res: Response) => {}
+  /*======  End of Estructura de un metodo  =====*/
+
+  constructor () {}
+
+  public alltemplates = async (req: Request, res: Response) => {
+		const response = await certificateService.alltemplates(req.getParameters.all())
+		return responseUtility.sendResponseFromObject(res, response)
+
+  }
+
+  public setCertificate = async (req: Request, res: Response) => {
+		const response = await certificateService.setCertificate(req.getParameters.all())
+		return responseUtility.sendResponseFromObject(res, response)
+
+  }
+}
+
+export const certificateController = new CertificateController();
+export { CertificateController as DefaultAdminCertificateCertificateController };
---
+++ src/app/routes/default/admin/certificate/certificateRoute.ts
@@ -0,0 +1,40 @@
+// @import_dependencies_node Import libraries
+// @end
+
+// @import_controller Import controller
+import { DefaultAdminCertificateCertificateController as Controller } from "@scnode_app/controllers/default/admin/certificate/certificateController";
+// @end
+
+// @import_utilities Import utilities
+import { routerUtility } from "@scnode_core/utilities/routerUtility";
+// @end
+
+class CertificateRoute {
+
+  private router_prefix: string = '/admin/certificate';
+
+  // @instance_controller
+  public instanceController: Controller = new Controller();
+  // @end
+
+  constructor () {}
+
+  /**
+  * Metodo que permite agregar rutas especificas por controlador
+  * @param app Objeto de clase Express Aplication
+  * @param [prefix] Prefijo para el enrutamiento
+  */
+  public routes(app,prefix: string = '/'): void {
+
+    const _route = `${prefix}${this.router_prefix}`;
+    routerUtility.get(app, _route, '/alltemplates', this.instanceController.alltemplates, [], ['auth'])
+    routerUtility.post(app, _route, '/create', this.instanceController.setCertificate, [], ['auth'])
+
+
+    // @add_routes Add routes: Ej: routerUtility.get(app,_route,'/url-for-request',this.instanceController.method,[{middleware: 'middleware-name', method: 'method-name'}...],[...]);
+    // @end
+  }
+}
+
+export const certificateRoute = new CertificateRoute();
+export { CertificateRoute as DefaultAdminCertificateCertificateRoute };
