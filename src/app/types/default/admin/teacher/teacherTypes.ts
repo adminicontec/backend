1061ecaaa7789bca674124faf63dbcc6e898c27e
Giovanni García <ggarcia.icontecpoligran.edu.co>
2021-11-20 22:50:50 -0500

    Elements to manipulate Teachers service.

--- src/app/types/default/admin/teacher/teacherTypes.ts
+++ src/app/types/default/admin/teacher/teacherTypes.ts
@@ -1,32 +1,44 @@
 // @import types
+import { IUser } from '@scnode_app/types/default/admin/user/userTypes'
 // @end
 
 
 // @add your types
-export interface ITeacher{
+export interface ITeacher {
+  userData?: IUser,
   user?: string // Identificador del usuario en campus
-  email: string,          // email de estudiante
-  password: string,
-  firstname: string,      // Nombre de estudiante
-  lastname: string,       // Apellido de estudiante
+  email?: string,          // email de estudiante
+  password?: string,
+  firstname?: string,      // Nombre de estudiante
+  lastname?: string,       // Apellido de estudiante
   documentType?: string,   // Tipo de documento (CC, CE, PAS )
   documentID?: string,     // Documento de identidad
   phoneNumber?: string // Numero de telefono
   rolename?: string,
   sendEmail?: boolean | 'true' | 'false' // Booleano que indica si se debe enviar notificación via email
   regional?: string,
-  city?:string,
-  country?:string,
-  contractType?:{
-    type?:string,
-    isTeacher?:boolean,
-    isTutor?:boolean
-  }
+  city?: string,
+  country?: string,
+
+  contractType?: {
+    type?: string,
+    isTeacher?: boolean,
+    isTutor?: boolean
+  },
+  courses?: any,
 
   id?: string             // Identificador del Enrollment
 }
 
-export interface IQualifiedProfessional{
+export interface ITeacherQuery {
+  pageNumber?: string, // Numero de pagina
+  nPerPage?: string, // Cantidad de elementos por pagina
+  select?: string, // Campos de la coleccion a buscar
+  search?: string, // Busca sobre los campos de la coleccion
+  roles?: Array<string> | string // Roles
+}
+
+export interface IQualifiedProfessional {
   documentID?: string,     // Documento de identidad
   email: string,          // email de estudiante
   modular: string,
@@ -40,9 +52,9 @@ export interface IQualifiedProfessional{
   specializations: string
 }
 
-export interface IMassiveLoad{
+export interface IMassiveLoad {
   sendEmail?: boolean | 'true' | 'false' // Booleano que indica si se debe enviar notificación via email
-  contentFile     :           // contento of file to be processed.
+  contentFile:           // contento of file to be processed.
   {
     name: string,
     data: Buffer,
