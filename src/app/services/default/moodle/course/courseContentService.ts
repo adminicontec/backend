9f66e944d05153e47079bf5fb908ae0a45469554
Giovanni García <ggarcia.icontecpoligran.edu.co>
2022-03-03 00:21:01 -0500

    When users update password on Campus, it will be synchronized with Moodle.

--- src/app/services/default/moodle/course/courseContentService.ts
+++ src/app/services/default/moodle/course/courseContentService.ts
@@ -147,7 +147,7 @@ class CourseContentService {
               sectionname: section.name,
               name: module.name,
               modname: module.modname,
-              isauditorquiz: (respMoodleModules.cm.idnumber.trim() == 'auditor') ? true : false,
+              isauditorquiz: (respMoodleModules.cm.idnumber) ? ((respMoodleModules.cm.idnumber.trim() == 'auditor') ? true : false) : false,
               instance: module.instance,
               visible: module.visible,
               uservisible: module.uservisible
