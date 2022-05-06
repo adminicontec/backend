abe94af1399557556c4bf5499aa7e44279bd94dc
Giovanni García <ggarcia.icontecpoligran.edu.co>
2022-05-06 12:27:26 -0500

    Fix missing references

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,6 +1,5 @@
 // @import_models
 import { CertificateLogsModel } from "@scnode_app/models/certificateLogsModel";
-import { PortfolioProgramModel } from "@scnode_app/models/portfolioProgramModel";
 import { DocumentQueueModel } from "@scnode_app/models/documentQueueModel";
 import { QualifiedTeachersModel } from "@scnode_app/models/qualifiedTeachersModel";
 import { SurveyLogModel } from "@scnode_app/models/surveyLogModel";
@@ -59,7 +58,6 @@ import { UserModel } from "@scnode_app/models/userModel";
 
 // @export_models
 export const CertificateLogs = CertificateLogsModel
-export const PortfolioProgram = PortfolioProgramModel
 export const DocumentQueue = DocumentQueueModel
 export const QualifiedTeachers = QualifiedTeachersModel
 export const SurveyLog = SurveyLogModel
