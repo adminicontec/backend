1d211e7aa7d9e92d77a9d597ee5cd0a739060c5f
Giovanni García Rodríguez <ggarcia.icontecpoligran.edu.co>
2022-07-06 08:34:58 -0500

    Add course stats for Teacher pt1

--- src/app/services/default/data/stats/statsService.ts
+++ src/app/services/default/data/stats/statsService.ts
@@ -116,6 +116,121 @@ class StatsService {
         stats = statGroup;
 
         // fill the missing months with '0' data
+        let index = 0;
+        for (let month of monthNameList) {
+          const found = statGroup.find(m => m.label === month);
+          if (!found) {
+            statGroup.push({ index: index++, label: month, data: 0 });
+          }
+          else {
+            index++;
+          }
+        }
+
+        statGroup.sort((a, b) => a.index - b.index);
+
+        for (let item of statGroup) {
+          report.labels.push(generalUtility.upperCaseString(item.label, false));
+          report.data.push(item.data);
+        }
+      }
+
+      return responseUtility.buildResponseSuccess('json', null, {
+        additional_parameters: {
+          currentYear: currentYear,
+          report: report
+        }
+      });
+
+    } catch (e) {
+      return responseUtility.buildResponseFailed('json')
+    }
+  }
+
+
+  /**
+   * Stats course service
+   * Permite consultar las cursos ejecutados por un docente en el año actual
+   */
+  public statsCourses = async (params: IStatsScheduledHoursQuery = {}) => {
+    let stats;
+    let report = { labels: [], data: [] };
+    let reducedTeacherScheduling = [];
+    let today = moment();
+    let currentYear = moment().year();
+    try {
+      let monthNameList = moment.localeData('es').monthsShort();
+
+      console.log("Stats for programmed courses:")
+      console.log(params);
+      console.log(`Current date: ${today}`);
+      console.log(`Current year: ${currentYear}`);
+
+      const respScheduledSessions: any = await courseSchedulingDetailsService.list({
+        teacher: params.teacher
+      });
+
+      if (respScheduledSessions.status == 'error') {
+        console.log(respScheduledSessions.message);
+        return responseUtility.buildResponseFailed('json', null, { error_key: { key: 'course_scheduling.details.not_found' } });
+      }
+
+      if (respScheduledSessions.schedulings && respScheduledSessions.schedulings.length > 0) {
+
+        console.log("=============================================");
+        for (let course of respScheduledSessions.schedulings) {
+          // avoid every Service in "Canceled" status
+          if (course.course_scheduling.schedulingStatus.name != 'Cancelado') {
+            for (let session of course.sessions) {
+
+              // filter every scheduled session of the current year
+              // avoid every scheduled session below the current date
+              // must adjust the Offset to real date record
+              let sessionStarDateOffset = moment(session.startDate.toISOString());
+
+              if (sessionStarDateOffset.isSame(today, 'year')) {
+                if (sessionStarDateOffset.isBefore(today)) {
+                  // console.log("========================================================");
+                  // console.log(`ServiceID: ${course.course_scheduling.metadata.service_id}`);
+                  // console.log(`course.course_scheduling: ${course.course_scheduling._id}`);
+                  // console.log("Time session: " + sessionStarDateOffset.local(true).format());
+
+                  reducedTeacherScheduling.push({
+                    course_scheduling: course.course_scheduling,
+                    course: course.course,
+                    startDate: course.startDate,
+                    number_of_sessions: course.number_of_sessions,
+                    duration: course.duration,
+                    duration_formated: course.duration_formated,
+                    session: {
+                      yearMonth: sessionStarDateOffset.local(true).format().substring(0, 7),
+                      monthNumber: moment(sessionStarDateOffset).month(),
+                      monthLabel: monthNameList[moment(sessionStarDateOffset).month()],
+                      startDate: sessionStarDateOffset.local(true).format(),
+                      duration: session.duration / 3600
+                    }
+                  });
+                }
+              }
+            }
+          }
+        }
+
+        reducedTeacherScheduling.sort((a, b) => a.session.startDate.localeCompare(b.session.startDate));
+
+        // group By Date ()
+        let statGroup = [];
+        reducedTeacherScheduling.reduce(function (res, value) {
+          if (!res[value.session.monthLabel]) {
+            res[value.session.monthLabel] = { index: value.session.monthNumber, label: value.session.monthLabel, data: 0 }
+            statGroup.push(res[value.session.monthLabel]);
+          }
+          res[value.session.monthLabel].data += value.session.duration;
+          return res;
+        }, {});
+        stats = statGroup;
+
+        // fill the missing months with '0' data
         console.log('------------------------------');
         console.log(monthNameList);
         let index = 0;
@@ -143,83 +258,8 @@ class StatsService {
 
       return responseUtility.buildResponseSuccess('json', null, {
         additional_parameters: {
-          report: report,
-          //reducedTeacherScheduling: reducedTeacherScheduling,
-          teacherScheduling: respScheduledSessions
-        }
-      });
-
-    } catch (e) {
-      return responseUtility.buildResponseFailed('json')
-    }
-  }
-
-
-
-  public statsCourses = async (params: IStatsScheduledHoursQuery = {}) => {
-    let stats;
-    let report = { labels: [], data: [] };
-    let reducedTeacherScheduling = [];
-    try {
-      console.log("Stats for programmed courses:")
-      console.log(params);
-
-      const respScheduledSessions: any = await courseSchedulingDetailsService.list({
-        teacher: params.teacher
-      });
-
-      if (respScheduledSessions.status == 'error') {
-        console.log(respScheduledSessions.message);
-        return responseUtility.buildResponseFailed('json', null, { error_key: { key: 'course_scheduling.details.not_found' } });
-      }
-
-      if (respScheduledSessions.schedulings && respScheduledSessions.schedulings.length > 0) {
-
-        respScheduledSessions.schedulings.sort((a, b) => a.startDate.localeCompare(b.startDate));
-
-        for (let course of respScheduledSessions.schedulings) {
-
-          for (let session of course.sessions) {
-
-            reducedTeacherScheduling.push({
-              course_scheduling: course.course_scheduling,
-              course: course.course,
-              startDate: course.startDate,
-              number_of_sessions: course.number_of_sessions,
-              duration: course.duration,
-              duration_formated: course.duration_formated,
-              session: {
-                yearMonth: session.startDate.toISOString().substring(0, 7),
-                startDate: session.startDate.toISOString().split('T')[0],
-                duration: session.duration / 3600
-              }
-            });
-          }
-        }
-
-        // group By Date ()
-        let statGroup = [];
-        reducedTeacherScheduling.reduce(function (res, value) {
-          if (!res[value.session.yearMonth]) {
-            res[value.session.yearMonth] = { label: value.session.yearMonth, data: 0 }
-            statGroup.push(res[value.session.yearMonth]);
-          }
-          res[value.session.yearMonth].data += value.session.duration;
-          return res;
-        }, {});
-        stats = statGroup;
-
-        for (let item of statGroup) {
-          report.labels.push(item.label);
-          report.data.push(item.data);
-        }
-
-      }
-
-      return responseUtility.buildResponseSuccess('json', null, {
-        additional_parameters: {
-          report: report //, //reducedTeacherScheduling
-          //teacherScheduling: reducedTeacherScheduling
+          currentYear: currentYear,
+          report: report
         }
       });
 
