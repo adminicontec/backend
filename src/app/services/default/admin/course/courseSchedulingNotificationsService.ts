ffe3a6fef7a8ca3b0ac69882e7af5074f55f437d
Nicolas Guerrero <nicolas.guerreros@amaris.com>
2022-07-14 08:51:22 -0500

    fix bug notifications

--- src/app/services/default/admin/course/courseSchedulingNotificationsService.ts
+++ src/app/services/default/admin/course/courseSchedulingNotificationsService.ts
@@ -517,14 +517,17 @@ class CourseSchedulingNotificationsService {
       console.log('::::::::::::::');
       console.log(auditorQuizModule);
       console.log('::::::::::::::');
-      let quizModuleData: IQuizModuleData = {
-        moduleName: auditorQuizModule.sectionname,
-        examnName: auditorQuizModule.name,
-        hasExam: true,
-        numberOfQuestions: 20
-      }
+      if (auditorQuizModule) {
+        let quizModuleData: IQuizModuleData = {
+          moduleName: auditorQuizModule.sectionname,
+          examnName: auditorQuizModule.name,
+          hasExam: true,
+          numberOfQuestions: 20
+        }
 
-      return quizModuleData;
+        return quizModuleData;
+      }
+      return null;
     } else {
       return null;
     }
