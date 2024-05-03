4a9dfdfdbfd4cac9d93c966832b37d81befa1692
Hernan Arregoces <developer@buendata.com>
2024-05-03 12:42:21 -0500

    add backend build 6

--- src/core/utilities/orm/ormMongodbUtility.ts
+++ src/core/utilities/orm/ormMongodbUtility.ts
@@ -50,7 +50,7 @@ class OrmMongodbUtility {
             options.sslCA = fs.readFileSync(connection_data.cert);
         }
         //const mongoUrl = `mongodb://${connection_data['host']}/${connection_data['dbname']}${(connection_data.options) ? connection_data.options : ''}`;
-        const mongoUrl = `mongodb+srv://mongodbicontec:GaLD8hHJymfKdiktoT9u@adminmongodbicontec.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retryWrites=false&maxIdleTimeMS=120000&maxTimeMS=600000&dbName=campusicontec`
+        const mongoUrl = `mongodb+srv://mongodbicontec:GaLD8hHJymfKdiktoT9u@adminmongodbicontec.mongocluster.cosmos.azure.com/campusicontec?tls=true&authMechanism=SCRAM-SHA-256&retryWrites=false&maxIdleTimeMS=120000`
         mongoose_global.Promise = global.Promise;
 
         await mongoose_global.connect(mongoUrl, options);
