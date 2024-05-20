99df45e51703103ed966cb23f2941f09a1013219
Hernan Arregoces <developer@buendata.com>
2024-05-20 08:20:54 -0500

    Fix crontab

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# Start cron
-cron
+
+# Start cron service
+service cron start
+
+# Tail the cron log file
+tail -f /var/log/cron.log &
+
 # Start Node.js application
 node ./src/app/server.js  # Replace with the name of your main server file
